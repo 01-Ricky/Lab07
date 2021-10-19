7b5a6471bf737c81240ef0c9c989d2a9705ee57d
AliasNull <fatezg47@gmail.com>
2021-10-19 20:52:46 +0800

    Added Jump Mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    private Rigidbody rb;
+    private const float jumpforce=5f;
+    
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+
+            Jump();
+        }
+       
+    }
+    private void Jump()
+    {
+        rb.velocity = Vector2.up * jumpforce;
+
     }
 }
